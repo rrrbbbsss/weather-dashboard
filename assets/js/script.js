2d4187848f3597702c6908ea349841cd1761cc2f
Royce Strange <roycest@swbell.net>
2022-07-04 01:38:17 -0500

    functions for getting weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,8 +67,59 @@ var uvCondition = function (uvi) {
     }
 };
 
-// search stuff
-// city list stuff
+var extractCityLocation = function (city) {
+    return {
+        lat: city[0].lat,
+        lon: city[0].lon,
+    };
+};
+var extractDayWeather = function (day) {
+    return {
+        date: dateString(day.dt),
+        icon: day.weather[0].icon,
+        temperature: (day.temp.day || day.temp),
+        humidity: day.humidity,
+        windspeed: day.wind_speed,
+        uvi: day.uvi,
+        uvCondition: uvCondition(day.uvi),
+    };
+};
+
+var getWeatherData = async function (city) {
+    // todo: make distinction between network errors and bad city name errors
+    // and do not save bad city names
+    try {
+        // get city latitute/longitude for weather lookup
+        var cityRequest = fetch(cityUrl(city));
+        var cityData = await cityRequest.then(getResponseJson);
+        var locationData = extractCityLocation(cityData);
+        // get weather data
+        var weatherRequest = fetch(weatherUrl(locationData.lat, locationData.lon));
+        var weatherData = await weatherRequest.then(getResponseJson);
+        //current data
+        var currentData = extractDayWeather(weatherData.current);
+        //forcast data
+        var forecastData = [];
+        // offset by one to start at next day's weather
+        for (var i = 1; i < dashboardData.forcastLength + 1; i++) {
+            forecastData.push(extractDayWeather(weatherData.daily[i]));
+        }
+        return {
+            city: city,
+            current: currentData,
+            forecast: forecastData
+        };
+    }
+    catch (error) {
+        return "there was an error somewhere";
+    }
+};
+
+/// search stuff ///
+/// city list stuff ///
+
+/// current weather stuff ///
+/// forecast stuff ///
 
 // current weather stuff
 // forecast stuff
